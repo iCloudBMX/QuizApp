47a6689f00577077e3372e7a37856e833a928b0b
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-04-10 15:02:27 +0500

    Exam, ExamAttendants and AttendantsAnswer Models Added

---
+++ src/QuizApp.Domain/Entities/AttendantsAnswer.cs
@@ -0,0 +1,15 @@
+﻿namespace QuizApp.Domain.Entities;
+
+public class AttendantsAnswer
+{
+    public Guid Id { get; set; }
+    public Guid ExamAttendantsId { get; set; }
+    public Guid QuestionId { get; set; }
+    public Guid OptionId { get; set; }
+    public Guid ExamId { get; set; }
+    public ExamAttendants ExamAttendants { get; set; }
+    //public Question Question { get; set; }
+    //public QuestionOption Option { get;set; }
+    public Exam Exam { get; set; }
+
+}
---
+++ src/QuizApp.Domain/Entities/Exam.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizApp.Domain.Entities;
+
+public class Exam
+{
+    public Guid Id { get; set; }
+    public Guid TesterId { get; set; }
+    public DateTime StartsAt { get; set; }
+    public DateTime EndsAt { get; set; }
+    public TimeOnly Duration { get; set; }
+    public string Link { get; set; }
+    public int QuestionCount { get; set; }
+    public User Tester { get; set; }
+    public ICollection<ExamAttendants> Attendants { get; set; }
+}
