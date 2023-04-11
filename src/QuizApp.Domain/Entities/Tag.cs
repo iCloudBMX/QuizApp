9ae60b26771400d3e64286155cb356c888148393
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-11 17:44:01 +0500

    Commented fields in class are uncommented

--- src/QuizApp.Domain/Entities/Tag.cs
+++ src/QuizApp.Domain/Entities/Tag.cs
@@ -4,8 +4,9 @@ public class Tag
 {
     public Guid Id { get; set; }
     public string Title { get; set; }
-    public Guid TesterId { get; set; }
 
+    public Guid TesterId { get; set; }
     public User User { get; set; }
+
     public ICollection<Question> Questions { get; set; }
 }
