fc74bbc167724898970baed642c4dd1cb54828dc
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-11 17:00:53 +0500

    Commented fields in class are uncommented

--- src/QuizApp.Domain/Entities/Question.cs
+++ src/QuizApp.Domain/Entities/Question.cs
@@ -5,15 +5,16 @@ namespace QuizApp.Domain.Entities;
 public class Question
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
     public QuestionTypes Type { get; set; }
     public QuestionLevel Level { get; set; }
     public string Content { get; set; }
     public DateTime CreatedAt { get; set; }
 
+    public Guid UserId { get; set; }
     public User Tester { get; set; }
+
     public ICollection<QuestionOption> QuestionOptions { get; set; }
     public ICollection<Tag> Tags { get; set; }
-    //public ICollection<Exam> Exams { get; set; }
-    //public ICollection<AttendsAnswer> MyProperty { get; set; }
+    public ICollection<Exam> Exams { get; set; }
+    public ICollection<AttendantsAnswer> AttendantsAnswers { get; set; }
 }
--- src/QuizApp.Domain/Entities/QuestionOption.cs
+++ src/QuizApp.Domain/Entities/QuestionOption.cs
@@ -3,10 +3,11 @@
 public class QuestionOption
 {
     public Guid Id { get; set; }
-    public Guid QuestionId { get; set; }
     public bool IsCorrect { get; set; }
     public string Content { get; set; }
 
-    //public ICollection<AttendantsAnswer> AttendantsAnswers { get; set; }
+    public Guid QuestionId { get; set; }
     public Question Question { get; set; }
+
+    public ICollection<AttendantsAnswer> AttendantsAnswers { get; set; }
 }
