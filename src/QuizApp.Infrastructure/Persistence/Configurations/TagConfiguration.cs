8276247c2da1344543c4fe40bf3746fe4f47032c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-11 17:43:45 +0500

    "Tag" configurations have been created

---
+++ src/QuizApp.Infrastructure/Persistence/Configurations/TagConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using QuizApp.Domain.Entities;
+
+namespace QuizApp.Infrastructure.Persistence.Configurations;
+public class TagConfiguration : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder
+            .ToTable(nameof(Tag));
+
+        builder
+            .HasKey(t => t.Id);
+
+        builder
+            .Property(t => t.Title)
+            .IsRequired(true);
+
+        builder
+            .HasOne(t => t.User)
+            .WithMany(u => u.Tags)
+            .HasForeignKey(t => t.TesterId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
