1f5a5f133be88f350b47272c9689f61c60c8f63e
Diyor <shumtakadiyor@gmail.com>
2023-04-12 17:19:04 +0500

    Exam Repository Added

---
+++ src/QuizApp.Infrastructure/Persistence/Repositories/ExamRepository.cs
@@ -0,0 +1,15 @@
+﻿using QuizApp.Domain.Entities;
+using QuizApp.Domain.Repositories;
+
+namespace QuizApp.Infrastructure.Persistence.Repositories
+{
+    public class ExamRepository : Repository<Exam>, IExamRepository
+    {
+        private readonly ApplicationDbContext aplicationDbContext;
+        public ExamRepository(ApplicationDbContext aplicationDbContext) :
+            base(aplicationDbContext)
+        {
+            this.aplicationDbContext = aplicationDbContext;
+        }
+    }
+}
