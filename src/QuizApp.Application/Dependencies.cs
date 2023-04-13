c59234ddc3b0cb4de11533f476b3f1c36b94f979
Diyor <shumtakadiyor@gmail.com>
2023-04-13 17:10:10 +0500

    DI Container

--- src/QuizApp.Application/Dependencies.cs
+++ src/QuizApp.Application/Dependencies.cs
@@ -1,5 +1,9 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using QuizApp.Application.AutoMapper;
+using QuizApp.Application.Exams.CreateExam;
+using QuizApp.Application.Exams.Validation;
 
 namespace QuizApp.Application;
 
@@ -9,6 +13,10 @@ public static class Dependencies
         this IServiceCollection services)
     {
         services.AddMediatR(typeof(Dependencies).Assembly);
+
+        services.AddAutoMapper(typeof(MappingProfile));
+
+        services.AddScoped<IValidator<CreateExamCommand>, ExamValidator>();
 
         return services;
     }
