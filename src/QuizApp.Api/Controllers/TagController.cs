fcd90285caed11777b05743ef6894fc9f1a0b806
Ravshanbek <axadovravshan@mail.ru>
2023-04-13 17:27:54 +0500

    Tag Controller

---
+++ src/QuizApp.Api/Controllers/TagController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizApp.Domain.Shared;
+
+namespace QuizApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ApiController
+    {
+        public TagController(ISender sender) : base(sender)
+        {
+        }
+    }
+}
