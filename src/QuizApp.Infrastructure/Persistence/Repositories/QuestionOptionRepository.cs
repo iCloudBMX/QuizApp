ec26b37fcd3adf56d0913624102f6517183ed8d9
Ravshanbek <axadovravshan@mail.ru>
2023-04-13 14:40:32 +0500

    QuestionOption and Tag Repositories are added

---
+++ src/QuizApp.Infrastructure/Persistence/Repositories/QuestionOptionRepository.cs
@@ -0,0 +1,15 @@
+﻿using QuizApp.Domain.Entities;
+using QuizApp.Domain.Repositories;
+
+namespace QuizApp.Infrastructure.Persistence.Repositories;
+
+internal class QuestionOptionRepository : Repository<QuestionOption>, IQuestionOptionRepository
+{
+    private readonly ApplicationDbContext applicationDbContext;
+
+    public QuestionOptionRepository(ApplicationDbContext applicationDbContext)
+        : base(applicationDbContext)
+    {
+        this.applicationDbContext = applicationDbContext;
+    }
+}
---
+++ src/QuizApp.Infrastructure/Persistence/Repositories/TagRepository.cs
@@ -0,0 +1,15 @@
+﻿using QuizApp.Domain.Entities;
+using QuizApp.Domain.Repositories;
+
+namespace QuizApp.Infrastructure.Persistence.Repositories;
+
+internal class TagRepository : Repository<Tag>,ITagRepository
+{
+    private readonly ApplicationDbContext applicationDbContext;
+
+    public TagRepository(ApplicationDbContext applicationDbContext)
+        : base(applicationDbContext)
+    {
+        this.applicationDbContext = applicationDbContext;
+    }
+}
