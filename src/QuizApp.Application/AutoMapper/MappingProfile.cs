fe472d6d7501f3171fd823c28c63d143bd175770
Diyor <shumtakadiyor@gmail.com>
2023-04-13 17:09:09 +0500

    Mapping

---
+++ src/QuizApp.Application/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using QuizApp.Application.Exams.CreateExam;
+using QuizApp.Domain.Entities;
+
+namespace QuizApp.Application.AutoMapper;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<CreateExamCommand, Exam>();
+
+	}
+}
