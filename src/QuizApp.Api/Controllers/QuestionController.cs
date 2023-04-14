e066daa0186c3a90025cd20dbc3ce3f5d5ca8946
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-14 17:54:47 +0500

    add  question controller

---
+++ src/QuizApp.Api/Controllers/QuestionController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizApp.Application.Questions.GetQuestionById;
+using QuizApp.Application.Users.GetUserById;
+using QuizApp.Domain.Shared;
+
+namespace QuizApp.Api.Controllers;
+[Route("api/[controller]")]
+public class QuestionController : ApiController
+{
+    public QuestionController(ISender sender) 
+        : base(sender)
+    { }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetQuestionById(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetQuestionByIdQuery(id);
+
+        Result<QuestionResponse> response = await Sender.Send(query, cancellationToken);
+
+        return 
+            response.IsSuccess ? Ok(response) : NotFound(response.Error);
+    }
+}
