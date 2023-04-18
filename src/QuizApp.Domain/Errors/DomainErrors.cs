8478f102032778f0bc5b2efabdc0d6849ac1db1c
Diyor <shumtakadiyor@gmail.com>
2023-04-18 15:53:22 +0500

    Add Exam.NotFound

--- src/QuizApp.Domain/Errors/DomainErrors.cs
+++ src/QuizApp.Domain/Errors/DomainErrors.cs
@@ -16,5 +16,9 @@ public static class DomainErrors
         public static readonly Func<string, Error> InvalidExamProperties = errors => new Error(
             code: "Exam properties is invalid",
             message: errors);
+
+        public static readonly Func<Guid, Error> NotFound = id => new Error(
+            code: "Exam.NotFound",
+            message: $"The exam with the identifier {id} was not found.");
     }
 }
