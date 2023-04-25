a28198e99302667df2dae8ca7d5c829cb83ebafa
Diyor <shumtakadiyor@gmail.com>
2023-04-25 16:22:14 +0500

    Error fixed

--- src/QuizApp.Api/Controllers/ExamsController.cs
+++ src/QuizApp.Api/Controllers/ExamsController.cs
@@ -4,45 +4,47 @@ using QuizApp.Application.Exams.CreateExam;
 using QuizApp.Application.Exams.GetExamById;
 using QuizApp.Domain.Shared;
 
-namespace QuizApp.Api.Controllers
+namespace QuizApp.Api.Controllers;
+
+[Route("api/exam")]
+public class ExamsController : ApiController
 {
-    [Route("api/exam")]
-    public class ExamsController : ApiController
+    public ExamsController(ISender sender)
+        : base(sender)
     {
-        public ExamsController(ISender sender)
-            : base(sender)
-        {
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateExam(
-            CreateExamCommand exam,
-            CancellationToken cancellationToken)
-        {
-            var response = await Sender.Send(exam, cancellationToken);
-
-            if (response.IsFailure)
-            {
-                return HandleFailure(response);
-            }
-
-            return Ok(response);
-        }
-
-        [HttpGet("{examId:guid}")]
-        public async Task<IActionResult> GetExamById(
-            Guid examId,
-            CancellationToken cancellationToken)
-        {
-            var query = new GetExamByIdQuery(examId);
-
-            Result<GetExamByIdResponse> response =
-                await Sender.Send(query, cancellationToken);
-
-            if (response.IsFailure)
-            {
-                return HandleFailure(response);
-            }
-
-            return Ok(response)}
     }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateExam(
+        CreateExamCommand exam,
+        CancellationToken cancellationToken)
+    {
+        var response = await Sender.Send(exam, cancellationToken);
+
+        if (response.IsFailure)
+        {
+            return HandleFailure(response);
+        }
+
+        return Ok(response);
+    }
+
+    [HttpGet("{examId:guid}")]
+    public async Task<IActionResult> GetExamById(
+        Guid examId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetExamByIdQuery(examId);
+
+        Result<GetExamByIdResponse> response =
+            await Sender.Send(query, cancellationToken);
+
+        if (response.IsFailure)
+        {
+            return HandleFailure(response);
+        }
+
+        return Ok(response);
+
+    }
+}
