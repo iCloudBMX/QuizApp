bcfaf96410a2615971421180f36b646b3568ed54
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-25 15:49:11 +0500

    add create and update mapper

--- src/QuizApp.Application/AutoMapper/MappingProfile.cs
+++ src/QuizApp.Application/AutoMapper/MappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using QuizApp.Application.Exams.CreateExam;
+using QuizApp.Application.Questions.CreateQuestion;
+using QuizApp.Application.Questions.UpdateQuestion;
 using QuizApp.Domain.Entities;
 
 namespace QuizApp.Application.AutoMapper;
@@ -9,6 +11,7 @@ public class MappingProfile : Profile
 	public MappingProfile()
 	{
 		CreateMap<CreateExamCommand, Exam>();
-
+		CreateMap<CreateQuestionCommand, Question>();
+		CreateMap<UpdateQuestionCommand, Question>();
 	}
 }
