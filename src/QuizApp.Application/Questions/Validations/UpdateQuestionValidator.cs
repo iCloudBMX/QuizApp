8631d1c51c3e2fb006ea0e35c403c0a3f41b0a60
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-04-26 05:49:25 +0500

    add update question validator

---
+++ src/QuizApp.Application/Questions/Validations/UpdateQuestionValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using QuizApp.Application.Questions.UpdateQuestion;
+
+namespace QuizApp.Application.Questions.Validations;
+public class UpdateQuestionValidator : AbstractValidator<UpdateQuestionCommand>
+{
+    public UpdateQuestionValidator()
+    {
+        
+    }
+}
