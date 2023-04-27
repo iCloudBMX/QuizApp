e56bdfbe8b506ed1fe744e9c981ba215ada6c359
ravshanbeek <ravshanaxadov@mail.ru>
2023-04-27 16:56:08 +0500

    Create Tag function added

---
+++ src/QuizApp.Application/Tags/CreateTag/CreateTagCommand.cs
@@ -0,0 +1,7 @@
+﻿using QuizApp.Application.Abstractions;
+using QuizApp.Application.Tags.GetTagById;
+
+namespace QuizApp.Application.Tags.CreateTag;
+
+public record CreateTagCommand (string Title, Guid TesterId) 
+    : ICommand<TagResponse>;
