a937bfae4e022ae469339f737b588d5fd9ebd9ec
ravshanbeek <ravshanaxadov@mail.ru>
2023-04-27 20:22:54 +0500

    delete tag added

---
+++ src/QuizApp.Application/Tags/DeleteTag/DeleteTagCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using QuizApp.Application.Abstractions;
+using QuizApp.Domain.Repositories;
+using QuizApp.Domain.Shared;
+
+namespace QuizApp.Application.Tags.DeleteTag;
+
+public class DeleteTagCommandHandler : ICommandHandler<DeleteTagCommand, Guid>
+{
+    private readonly ITagRepository tagRepository;
+    private readonly IUnitOfWork unitOfWork;
+
+
+    public DeleteTagCommandHandler(
+        ITagRepository tagRepository,
+        IUnitOfWork unitOfWork = null)
+    {
+        this.tagRepository = tagRepository;
+        this.unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Guid>> Handle(DeleteTagCommand request, CancellationToken cancellationToken)
+    {
+        var tag = tagRepository.SelectAsync(cancellationToken, request.TagId);
+        throw new NotImplementedException();
+    }
+}
