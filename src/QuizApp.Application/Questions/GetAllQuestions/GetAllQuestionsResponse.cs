308b7a5e4c1de8f30ba161cf03f924057a378b6a
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-05-01 18:05:53 +0500

    add getallquestionResponse

---
+++ src/QuizApp.Application/Questions/GetAllQuestions/GetAllQuestionsResponse.cs
@@ -0,0 +1,5 @@
+﻿using QuizApp.Application.Questions.GetQuestionById;
+
+namespace QuizApp.Application.Questions.GetAllQuestions;
+public record GetAllQuestionsResponse(
+    IList<GetQuestionByIdResponse> Allquestion);
