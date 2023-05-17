17b039219258af02476817b1a5a897128be8cdf2
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-05-17 21:02:45 +0500

    conflicts resolved

--- src/QuizApp.Application/Users/Login/LoginQuery.cs
+++ src/QuizApp.Application/Users/Login/LoginQuery.cs
@@ -4,5 +4,5 @@ namespace QuizApp.Application.Users.Login
 {
     public record LoginQuery(
         string Email,
-        string Password) : IQuery<LoginResponse>;
+        string Password) : IQuery<LoginQueryResponse>;
 }
