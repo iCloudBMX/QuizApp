6a55604dcfa630e34ebc2af0e233778989dce3a8
ravshanbeek <ravshanaxadov@mail.ru>
2023-05-17 10:59:55 +0500

    Generate Random salt func is added

---
+++ src/QuizApp.Application/Helpers/GenerateSalt/GenerateSalt.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace QuizApp.Application.Helpers.GenerateSalt;
+
+public class GenerateSalt : IGenerateSalt
+{
+    static string charList = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+    string IGenerateSalt.GenerateSalt()
+    {
+        Random random = new Random();
+        StringBuilder randomSalt = new StringBuilder();
+
+        for (int i = 0; i < 64; i++)
+        {
+            randomSalt
+                .Append(charList[random.Next(charList.Length)]);
+        }
+
+        return randomSalt.ToString();
+    }
+}
