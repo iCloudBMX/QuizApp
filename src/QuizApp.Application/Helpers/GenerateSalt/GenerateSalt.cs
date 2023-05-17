1c518869057c2c117165764c842200fcdaed70cc
ravshanbeek <ravshanaxadov@mail.ru>
2023-05-17 15:47:06 +0500

    generateSalt chenged to generate string

--- src/QuizApp.Application/Helpers/GenerateSalt/GenerateSalt.cs
+++ src/QuizApp.Application/Helpers/GenerateSalt/GenerateSalt.cs
@@ -5,12 +5,13 @@ namespace QuizApp.Application.Helpers.GenerateSalt;
 public class GenerateSalt : IGenerateSalt
 {
     static string charList = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
-    string IGenerateSalt.GenerateSalt()
+    const int stringLength = 64;
+    public string GenerateStrng()
     {
         Random random = new Random();
         StringBuilder randomSalt = new StringBuilder();
 
-        for (int i = 0; i < 64; i++)
+        for (int i = 0; i < stringLength; i++)
         {
             randomSalt
                 .Append(charList[random.Next(charList.Length)]);
